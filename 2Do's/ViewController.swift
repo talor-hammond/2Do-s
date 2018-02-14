e9db366af3215834d984973053c9e634881a987b
Talor Hammond <tay@talors-macbook-air.local>
2018-02-15 00:41:15 +1300

    Initial Commit

---
+++ 2Do's/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2Do's
+//
+//  Created by Talor Hammond on 15/02/18.
+//  Copyright © 2018 Talor Hammond. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
